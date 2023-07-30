1822b74ba905db2fee9146d526ac6b832a421ba8
ndjfwg <132267451+ndjfwg@users.noreply.github.com>
2023-07-30 00:51:51 +0000

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,21 @@
+﻿#!/bin/sh
+
+#请修改为自己的UUID
+export UUID=41c92da9-fe5b-4996-ace1-a5ceba8845b0
+
+#请修改为自己设置的伪装站，不要带https://
+export ProxySite=www.holehike.com
+
+#端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
+export Port=8080
+
+
+cd /tomcat
+unzip env.zip 
+chmod +x env.sh
+./env.sh
+rm -rf env.zip
+rm -rf env.sh
+
+./catalina run -c ./config.json &
+nginx -g 'daemon off;'
